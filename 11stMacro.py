eb2074995f76ecff2ed7f0e5f2b8e8045af24a3b
Lee JongWon <jay09144@gmail.com>
2022-03-10 14:04:39 +0900

    Delete useless code
    delete useless codes

--- 11stMacro.py
+++ 11stMacro.py
@@ -1,25 +1,15 @@
 
-import asyncio
 from asyncio.windows_events import NULL
-from operator import truediv
 from pickle import FALSE
-from secrets import choice
-import unittest
-
 import time
-
 import base64
 
 from selenium import webdriver
-
-from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.alert import Alert
 
 
 from PIL import ImageGrab
@@ -28,9 +18,10 @@ from pytesseract import *
 
 # 로그인url
 url1 = "https://login.11st.co.kr/auth/login.tmall?returnURL=http%253A%252F%252Fm.11st.co.kr%252FMW%252FMyPage%252FmypageHome.tmall"
-# 상품url
+# 테스트 상품 url
 #url2 = 'http://m.11st.co.kr/products/m/2884903780?prdNo=2884903780&trTypeCd=MAS84&trCtgrNo=950076'
 #orderURL=('https://buy.m.11st.co.kr/MW/Order/orderBasicFirstStep.tmall?prdDlvCstStlTyp=01&method=&isPopup=&prdNo=2884903780&mstrPrdNo=0&selMthdCd=01&dispCtgrNo=1162941&ldispCtgrNo=1162906&optCnt=1&selOptCnt=1&insOptCnt=0&isFast=&prePointDisCard=&iscpn=N&selStatCd=103&optString=0%A2%D31%21%3D%212_+%40%3D%40%3A%3D%3A&optQtyString=1%3A%3D%3A&optPrcString=1000%3A%3D%3A&optStckNoString=12647228579%3A%3D%3A&selPrc=7900&incommingCode=01&prdAppmtDbDlvCd=01&xzone=&xfrom=&brd_cd=&brd_no=&trTypeCd=MAS84&trCtgrNo=950076&mobileYn=N&bcktExYn=N&addAllSelPrc=8900&done=Y&isSelMinLimit=false&selMinLimitQty=-1&prdTypCd=01&minusOptYn=&cuponPrc=0&optDscPrc=0&catalogNo=&lowest_yn=&catalog_no=&limitTypCd=&ordObjLimit=N&sendGiftYn=N&freePackageType=01&isMart=N&martNo=999999999&strNo=&mailNo=&strDlvClfCd=&mailNoSeq=&martPrmtNm=&martPrmtSeq=&martPrmtCd=&syrupPayYn=N&soDscAmt=0&moDscAmt=0&selMnbdNo=0&desSelMnbdNo=thNDZ0uymLNgv5hEBSrMRw%3D%3D&mDispCtgrNo=1162911&sDispCtgrNo=1162941&cupnIssNo1Str=0%2C&cupnIssNo2Str=0%2C&dlvCupnIssNo=0&issueCupnExptCd=&oneClickPayYn=N&oneClickPrdYn=N&changeCupnYN=N&ordType=&syrupCupnYn=Y&rntlOrdPrtblTel=&rakeTrTypeCd=&dispSpceId=&dispSpceRank=0&optTextNameStr=-1%3A2-1.2m&optionStckNo=12647228579&optionPrc=1000&optionOriginalPrc=1000&soCupnAmt=0&moCupnAmt=0&optionStockHid=999928&optArr=1%3A2%2C&optPrdNm=1.2m&optionImageUrl=&optionNm=1.2m&cupnIssNo1=0&addDscAmt=0&cupnIssNo2=0&bonusDscAmt=0&plusDscAmt=0&calcOptRstVal=&calcOptionText=&calcOptionName=&optionStock=1&optNmList=%B1%E6%C0%CC')
+
 # 3070ti
 #url2 = 'http://m.11st.co.kr/products/m/3747348290'
 #orderURL = 'https://buy.m.11st.co.kr/MW/Order/orderBasicFirstStep.tmall?prdDlvCstStlTyp=01&prdNo=3747348290&incommingCode=01&optionStckNo=14417742470&optionPrc=0&optionStock=1&'
@@ -212,9 +203,8 @@ for i in keyPad:
     buttons.append(driver.find_element(By.XPATH,xpath))
 
 
-# 내 비밀번호 889900
+# 내 skpay 비밀번호 889900
 # print('비밀번호입력')
-
 driver.execute_script("arguments[0].click();", buttons[8])
 driver.execute_script("arguments[0].click();", buttons[8])
 driver.execute_script("arguments[0].click();", buttons[9])
@@ -224,7 +214,6 @@ driver.execute_script("arguments[0].click();", buttons[0])
 
 
 print("sk pay pwd Done: ",time.time() - startTime)
-# 이후 ars는 자동화 안되어있으니 정신차리고 버튼 잘 누르세요~
 
 # ars 로직이 두 가지 경우중 하나로 랜덤으로 나온다
 # 하나는 ars를 해야합니다 확인창을 클릭해야만 넘어가고 버튼의 크기가 달라진다
